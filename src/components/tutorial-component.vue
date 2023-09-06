4909bb25d2beaf3678da834b8a563dc2580884fd
nreupomckinsey <naldo_reupo-musayon@mckinsey.com>
2023-09-05 20:33:54 -0500

    Feature(learning-center) setup i18n

--- src/components/tutorial-component.vue
+++ src/components/tutorial-component.vue
@@ -1,8 +1,10 @@
 <template>
     <label class="title" aria-roledescription="title">{{$t('tutorial')}} {{title}}</label>
 
-    <label for="contador">Count:</label>
-    <input type="text" id="contador">
+    <label for="count">Count:</label>
+    <input type="text" id="count" aria-label="This field is for the quantity of tutorials on stock">
+
+    <img src="cat.jpg" alt="this image show a cat " aria-roledescription="click here to go the datails of the cat">
 </template>
 
 <script>
