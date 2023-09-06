dfe586f7178a4d7c4606ec4a95d665ab20d6ea0b
nreupomckinsey <naldo_reupo-musayon@mckinsey.com>
2023-09-05 20:44:25 -0500

    Feature(learning-center) setup i18n

--- src/components/tutorial-component.vue
+++ src/components/tutorial-component.vue
@@ -4,6 +4,14 @@
     <label for="count">Count:</label>
     <input type="text" id="count" aria-label="This field is for the quantity of tutorials on stock">
 
+
+    <label for="count">date of expiration:</label>
+    <input type="date">
+
+
+    <label for="count" for="quantity">Quantity:</label>
+    <input type="number" id="quantity">
+
     <img src="cat.jpg" alt="this image show a cat " aria-roledescription="click here to go the datails of the cat">
 </template>
 
